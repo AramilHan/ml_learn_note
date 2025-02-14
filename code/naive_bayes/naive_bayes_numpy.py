663263fe40a7b075eb0027ac63f9b865e6647038
hanjingwei <978645649@qq.com>
2025-02-14 22:21:01 +0800

    fix 朴素贝叶斯

---
+++ code/naive_bayes/naive_bayes_numpy.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+@Author  : -
+@Time    : 2025/2/14 22:20
+@File    : naive_bayes_numpy.py
+@Description : 
+"""
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    exit(main())
